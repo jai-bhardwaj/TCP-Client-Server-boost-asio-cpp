7b428f8482cf081e9d34a7e26bf316611903243f
Sujal Sharma <0987sujals@gmail.com>
2023-06-05 13:38:05 +0530

    Update in the casting of the len variable.

--- MOYFClient/main.cpp
+++ MOYFClient/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
             std::array<char, 128> buf{};
             boost::system::error_code error;
 
-            size_t len = socket.read_some(boost::asio::buffer(buf), error);
+            auto len = socket.read_some(boost::asio::buffer(buf), error);
 
             if (error == boost::asio::error::eof) {
                 // Clean connection cut-off
@@ -27,10 +27,8 @@ int main(int argc, char* argv[]) {
             } else if (error) {
                 throw boost::system::system_error(error);
             }
-            std::cout.write(buf.data(), len);
+            std::cout.write(buf.data(), static_cast<std::streamsize>(len));
         }
-
-
     } catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
     }
